ff37645a713849765958cf291dfabd11c34e1ec3
stekarag <steliouras@yahoo.com>
2021-06-12 20:29:26 +0300

    Update secondsConverter.py
    Removed log output, standardized standard output.

--- secondsConverter.py
+++ secondsConverter.py
@@ -9,34 +9,27 @@ syear = 29030400
 t = [0,0,0,0,0,0,0]
 
 def convert(s, tm=t):
-    print("s is %i"%s)
     if s >= syear:
-        print("%i greater than a year"%s)
         sy = s//syear
         s = s%syear
         t[0] = sy
     elif s >= smonth:
-        print("%i greater than a month"%s)
         sm = s//smonth
         s = s%smonth
         t[1] = sm
     elif s >= sweek:
-        print("%i greater than a week"%s)
         sw = s//sweek
         s = s%sweek
         t[2] = sw
     elif s >= sday:
-        print("%i greater than a day"%s)
         sd = s//sday
         s = s%sday
         t[3] = sd
     elif s >= shour:
-        print("%i greater than an hour"%s)
         sh = s//shour
         s = s%shour
         t[4] = sh
     elif s >= smin:
-        print("%i greater than a minute"%s)
         smi = s//smin
         s = s%smin
         t[5] = smi
@@ -45,14 +38,12 @@ def convert(s, tm=t):
         s = 0
         t[6] = ss
     elif s < 1:
-        print("%i smaller than a second"%s)
         pass
     
     if s >= 1:
        convert(s,t)
 
     return t
-    print("convert exiting")
 
 b = 0
 while (b is 0):
@@ -62,6 +53,10 @@ while (b is 0):
     except NameError:
         print("Please enter a valid number.")
         b = 0
+    except ValueError:
+        print("Please enter a valid decimal number.")
+
+        b = 0
 if sec < 0:
     sec = abs(sec)
     print("Negative number detected, using absolute value")
@@ -70,4 +65,14 @@ print("Calculating %i"%sec)
 
 tm = convert(sec)
 
-print("%i years, %i months, %i weeks, %i days, %i hours, %i minutes and %i seconds"%(tm[0],tm[1],tm[2],tm[3],tm[4],tm[5],tm[6]))
+answer = ""
+ay = "" if tm[0] is 0 else "%i year(s), "%tm[0]
+am = "" if tm[1] is 0 else "%i month(s), "%tm[1]
+aw = "" if tm[2] is 0 else "%i week(s), "%tm[2]
+ad = "" if tm[3] is 0 else "%i day(s), "%tm[3]
+ah = "" if tm[4] is 0 else "%i hour(s), "%tm[4]
+ami = "" if tm[5] is 0 else "%i minute(s), "%tm[5]
+ase = "" if tm[6] is 0 else "%i second(s)."%tm[6]
+answer = ay+am+aw+ad+ah+ami+ase
+
+print(answer)
