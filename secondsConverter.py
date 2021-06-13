7e2fd27dd5e91eb58ffdd1f073d1274b092adf1c
stekarag <steliouras@yahoo.com>
2021-06-13 21:26:47 +0300

    Update secondsConverter.py

--- secondsConverter.py
+++ secondsConverter.py
@@ -45,6 +45,19 @@ def convert(s, tm=t):
 
     return t
 
+def formatAns(tm=[0,0,0,0,0,0,0]):
+    answer = "Answer is "
+    ay = "" if tm[0] is 0 else "%i year(s), "%tm[0]
+    am = "" if tm[1] is 0 else "%i month(s), "%tm[1]
+    aw = "" if tm[2] is 0 else "%i week(s), "%tm[2]
+    ad = "" if tm[3] is 0 else "%i day(s), "%tm[3]
+    ah = "" if tm[4] is 0 else "%i hour(s), "%tm[4]
+    ami = "" if tm[5] is 0 else "%i minute(s), "%tm[5]
+    ase = "%i second(s)."%tm[6]
+    answer = answer+ay+am+aw+ad+ah+ami+ase
+    
+    return answer
+
 b = 0
 while (b is 0):
     try:
@@ -65,13 +78,6 @@ print("Converting %i'' to readable form."%sec)
 
 tm = convert(sec)
 
-answer = ""
-ay = "" if tm[0] is 0 else "%i year(s), "%tm[0]
-am = "" if tm[1] is 0 else "%i month(s), "%tm[1]
-aw = "" if tm[2] is 0 else "%i week(s), "%tm[2]
-ad = "" if tm[3] is 0 else "%i day(s), "%tm[3]
-ah = "" if tm[4] is 0 else "%i hour(s), "%tm[4]
-ami = "" if tm[5] is 0 else "%i minute(s), "%tm[5]
-ase = "" if tm[6] is 0 else "%i second(s)."%tm[6]
-answer = ay+am+aw+ad+ah+ami+ase
+answer = formatAns(tm)
+
 print(answer)
