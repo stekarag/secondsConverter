637e77e3b3201e3ece7e62737b07a017de1156ae
stekarag <steliouras@yahoo.com>
2021-07-30 23:29:35 +0300

    Update and rename secondsConverter.py to seconds_converter.py
    More pythonic now

--- seconds_converter.py
+++ seconds_converter.py
@@ -6,32 +6,33 @@ sday = 86400
 sweek = 604800
 smonth = 2419200
 syear = 29030400
-t = [0,0,0,0,0,0,0]
+t = [0, 0, 0, 0, 0, 0, 0]
+
 
 def convert(s, tm=t):
     if s >= syear:
         sy = s//syear
-        s = s%syear
+        s = s % syear
         t[0] = sy
     elif s >= smonth:
         sm = s//smonth
-        s = s%smonth
+        s = s % smonth
         t[1] = sm
     elif s >= sweek:
         sw = s//sweek
-        s = s%sweek
+        s = s % sweek
         t[2] = sw
     elif s >= sday:
         sd = s//sday
-        s = s%sday
+        s = s % sday
         t[3] = sd
     elif s >= shour:
         sh = s//shour
-        s = s%shour
+        s = s % shour
         t[4] = sh
     elif s >= smin:
         smi = s//smin
-        s = s%smin
+        s = s % smin
         t[5] = smi
     elif s >= 1:
         ss = s
@@ -39,24 +40,26 @@ def convert(s, tm=t):
         t[6] = ss
     elif s < 1:
         pass
-    
+
     if s >= 1:
-       convert(s,t)
+       convert(s, t)
 
     return t
 
-def formatAns(tm=[0,0,0,0,0,0,0]):
+
+def format_ans(tm=[0, 0, 0, 0, 0, 0, 0]):
     answer = "Answer is "
-    ay = "" if tm[0] is 0 else "%i year(s), "%tm[0]
-    am = "" if tm[1] is 0 else "%i month(s), "%tm[1]
-    aw = "" if tm[2] is 0 else "%i week(s), "%tm[2]
-    ad = "" if tm[3] is 0 else "%i day(s), "%tm[3]
-    ah = "" if tm[4] is 0 else "%i hour(s), "%tm[4]
-    ami = "" if tm[5] is 0 else "%i minute(s), "%tm[5]
-    ase = "%i second(s)."%tm[6]
+    ay = "" if tm[0] is 0 else " % i year(s), " % tm[0]
+    am = "" if tm[1] is 0 else " % i month(s), " % tm[1]
+    aw = "" if tm[2] is 0 else " % i week(s), " % tm[2]
+    ad = "" if tm[3] is 0 else " % i day(s), " % tm[3]
+    ah = "" if tm[4] is 0 else " % i hour(s), " % tm[4]
+    ami = "" if tm[5] is 0 else " % i minute(s), " % tm[5]
+    ase = "%i second(s)." % tm[6]
     answer = answer+ay+am+aw+ad+ah+ami+ase
     
     return answer
+
 
 b = 0
 while (b is 0):
@@ -74,10 +77,9 @@ if sec < 0:
     sec = abs(sec)
     print("Negative number detected, using absolute value.")
 
-print("Converting %i'' to readable form."%sec)
+print("Converting %i'' to readable form." % sec)
 
 tm = convert(sec)
-
-answer = formatAns(tm)
+answer = format_ans(tm)
 
 print(answer)
